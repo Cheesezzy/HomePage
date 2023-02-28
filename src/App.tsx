a1e311276f896d9d4497c9e4bbfe50cd3853a555
cheesezzy <chi.patrick0000@gmail.com>
2023-02-28 09:35:20 +0100

    adjust component styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Top from './components/Top'
 import CarouselSection from './pageSections/CarouselSection'
 import CircleSection from './pageSections/CircleSection'
 import HeroSection from './pageSections/HeroSection'
@@ -14,6 +15,7 @@ function App() {
 
   return (
     <div style={{ width:"100%"}}>
+        <Top />
         <Header/>
         <HeroSection />
         <CircleSection />
